eaf512a0d0c3956e94664b9a2b0417e1e59c7dfe
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 22:08:21 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1,6 +1,4 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 // errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -2116,7 +2114,7 @@ var CodeMirror = {};
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js" integrity="sha512-xQBQYt9UcgblF6aCMrwU1NkVA7HCXaSN2oq0so80KO+y68M+n64FOcqgav4igHe6D5ObBLIf68DWv+gfBowczg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
       document.getElementsByTagName('form')[0].addEventListener('submit', function(){
-      this.action = '/index.php';
+      this.action = '<?php echo isset($_GET['logout']) ? strtok($_SERVER['REQUEST_URI'], '?') : $_SERVER['REQUEST_URI']; ?>';
       this.method = 'post';
       console.log("runned")
     }, false);
