2efaa9e508194e95fde671dceac868c2e99b6b05
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 18:05:05 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1,4 +1,6 @@
 <?php
+header("access-control-allow-origin: *");
+
 // errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -1696,19 +1698,6 @@ if(post('action')){
 
 	  // required for some browsers
 	  if(@ini_get('zlib.output_compression')) @ini_set('zlib.output_compression', 'Off');
-
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-      header('Access-Control-Allow-Origin: *');
-      header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-      header('Access-Control-Allow-Headers: token, Content-Type');
-      header('Access-Control-Max-Age: 1728000');
-      header('Content-Length: 0');
-      header('Content-Type: text/plain');
-      // die();
-    }
-    
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
 
 	  // headers
 	  header('Content-Description: File Transfer');
