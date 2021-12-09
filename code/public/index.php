66df81fd25168c0154327a51b059c8cccd04bcf4
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 18:39:19 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1995,25 +1995,6 @@ header('files-msg: [' . header_memory_time() . ']');
 
 // htmlstart
 ?>
-
-<?php
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-  header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Max-Age: 86400'); // cache for 1 day
-
-}
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-  }
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-      header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-  }
-  exit(0);
-}
-?>
 <!doctype html>
 <html<?php echo ' class="menu-' . ($menu_enabled ? 'enabled' : 'disabled sidebar-closed') . '"'; ?>>
   <head>
@@ -2028,7 +2009,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
   </head>
 
   <body class="body-loading"><svg viewBox="0 0 18 18" class="svg-preloader svg-preloader-active preloader-body"><circle cx="9" cy="9" r="8" pathLength="100" class="svg-preloader-circle"></svg>
-  <form action="index.php" method="post">  
+  <form action="/index.php" method="post">  
   <main id="main">
     
       <?php
