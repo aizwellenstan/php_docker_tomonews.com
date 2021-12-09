d7174995a256de82348ce226e2f0b701075d86e3
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 22:04:47 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 // errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
