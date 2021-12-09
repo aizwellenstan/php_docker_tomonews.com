2857e7c2f75966f65bc85c16351d2803548188c2
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 18:01:07 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -325,12 +325,6 @@ class config {
   }
 };
 
-header('Access-Control-Allow-Origin: *');
-
-header('Access-Control-Allow-Methods: GET, POST');
-
-header("Access-Control-Allow-Headers: X-Requested-With");
-
 // login page
 function login_page($is_login_attempt, $sidx, $is_logout, $client_hash){
 ?>
@@ -1702,6 +1696,19 @@ if(post('action')){
 
 	  // required for some browsers
 	  if(@ini_get('zlib.output_compression')) @ini_set('zlib.output_compression', 'Off');
+
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      header('Access-Control-Allow-Origin: *');
+      header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+      header('Access-Control-Allow-Headers: token, Content-Type');
+      header('Access-Control-Max-Age: 1728000');
+      header('Content-Length: 0');
+      header('Content-Type: text/plain');
+      // die();
+    }
+    
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
 
 	  // headers
 	  header('Content-Description: File Transfer');
