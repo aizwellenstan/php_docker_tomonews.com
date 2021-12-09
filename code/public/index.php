28216dc1fd5b2d8b2b956afe8e7d32f755204712
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 19:56:56 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1756,7 +1756,7 @@ if(post('action')){
         //     $zip_file = config::$storage_path . '/zip/' . $zip_file_name;
         // }
 
-        $config_storage_path = '/var/www/html/public/_files';
+        $config_storage_path = '/var/www/html/public';
 
         $zip_file_name = basename($dir) . '.' . substr(md5($dir), 0, 6) . '.zip';
         $zip_file = $config_storage_path. '/zip/' . $zip_file_name;
