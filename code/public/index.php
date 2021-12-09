fc3e142249d06ff0203b73e2514ebe79252fb1d7
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 18:21:08 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1,7 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
 // errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
