6e464eff4a922989c432c177af97e9adc750a1f5
aizwellenstan <aizwellenstan@gmail.com>
2021-12-09 18:36:48 +0900

    lts

--- code/public/index.php
+++ code/public/index.php
@@ -1,21 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-  header('Access-Control-Allow-Credentials: true');
-  header('Access-Control-Max-Age: 86400'); // cache for 1 day
-
-}
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-  }
-  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-      header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-  }
-  exit(0);
-}
 // errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -2011,6 +1994,25 @@ if(config::$config['allow_upload']) {
 header('files-msg: [' . header_memory_time() . ']');
 
 // htmlstart
+?>
+
+<?php
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  header('Access-Control-Allow-Credentials: true');
+  header('Access-Control-Max-Age: 86400'); // cache for 1 day
+
+}
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+      header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  }
+  if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+      header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  }
+  exit(0);
+}
 ?>
 <!doctype html>
 <html<?php echo ' class="menu-' . ($menu_enabled ? 'enabled' : 'disabled sidebar-closed') . '"'; ?>>
